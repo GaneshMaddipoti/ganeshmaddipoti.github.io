90bdaf71cf1a521fc71fba25f99208e70a5c2736
D638011 <ganesh.maddipoti@jpmchase.com>
2023-11-03 11:06:01 +0000

    everest2

---
+++ js/data-platform.js
@@ -0,0 +1,10 @@
+let dataPFNodeDataArray = [
+
+    {key: "Data Platform", color: "WhiteSmoke", isGroup: true, category: "tree", img: "assets/img/gen/gen-db.svg"},
+    {key: "Start", group:"Cloud Computing", category: "picTemplate",},
+
+];
+
+let dataPFLinkDataArray = [
+
+];
---
+++ js/everest2.js
@@ -0,0 +1,31 @@
+let cloudComputingHTML = `<b>What is</b>
+            Cloud computing is the on-demand delivery of IT resources over the Internet with pay-as-you-go pricing. 
+            Instead of buying, owning, and maintaining physical data centers and servers, you can access technology services, 
+            such as computing power, storage, and databases, on an as-needed basis from a cloud provider like AWS, GCP, Azure. 
+            <b>Who is using</b>
+            Organizations of every type, size, and industry are using the cloud for a wide variety of use cases, 
+            such as data backup, software development and testing, big data analytics, and customer-facing web applications. 
+            <b>Benefits</b>
+            Agility 
+            Elasticity 
+            Cost Savings 
+            Deploy Globally in Minutes 
+            <b>Types</b>
+            Infrastructure as a service (IaaS) 
+            Platform as a service (PaaS) 
+            Software as a service (SaaS) 
+        `;
+
+
+
+let nodeDataArray = [
+
+    {key: "Cloud Computing", color: "WhiteSmoke", isGroup: true, category: "tree", img: "assets/img/gen/gen-cloud.svg", toolTipHTML: cloudComputingHTML},
+
+
+];
+
+let linkDataArray = [
+
+
+];
